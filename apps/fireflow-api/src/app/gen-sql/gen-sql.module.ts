6c0654f1ac3ce79316e6e224bd498c75baefbaf8
Buddhi Kavindra <visitbudkavin@gmail.com>
2026-03-02 21:52:01 +0530

    Reset project

--- apps/fireflow-api/src/app/gen-sql/gen-sql.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GenSqlService } from './gen-sql.service';
-import { GenSqlController } from './gen-sql.controller';
-
-@Module({
-  controllers: [GenSqlController],
-  providers: [GenSqlService],
-})
-export class GenSqlModule {}
